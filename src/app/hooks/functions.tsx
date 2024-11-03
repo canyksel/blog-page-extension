9521505af4c2fd9e3e6c9ddddb73e66cd7e82313
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-11-03 15:07:24 +0300

    fix: update comment at calculate reading time func

--- src/app/hooks/functions.tsx
+++ src/app/hooks/functions.tsx
@@ -1,11 +1,12 @@
 export function calculateReadingTime(textLength: number) {
-    //Average reading speed (words per minute)
-    const wordsPerSecond = 3.96;
-    //Calculate reading time as minute
-    const totalReadingTime = Math.ceil(textLength / wordsPerSecond);
-  
-    const readingMinute = (totalReadingTime - (totalReadingTime % 60)) / 60;
-    const readingSecond = totalReadingTime % 60;
-  
-    return `${readingMinute} min ${readingSecond} sec read`;
-  }
+  //Average reading speed (words per minute)
+  const wordsPerSecond = 3.96;
+  //Calculate total reading time
+  const totalReadingTime = Math.ceil(textLength / wordsPerSecond);
+  //Calculate reading time as minute
+  const readingMinute = (totalReadingTime - (totalReadingTime % 60)) / 60;
+  //Calculate reading time as second
+  const readingSecond = totalReadingTime % 60;
+
+  return `${readingMinute} min ${readingSecond} sec read`;
+}
